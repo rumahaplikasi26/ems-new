7dcde0ae4a433327dd9d6350522f76f009cded67
rumahaplikasi26 <trimitramediateknologi@gmail.com>
2025-09-30 20:05:40 +0700

    [add] Add Calculate Working Hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('app:calculate-working-hourse')->dailyAt('00:00');
-        $schedule->command('app:calculate-working-hourse')->everyMinute();  // For testing
+        $schedule->command('app:calculate-working-hourse')->dailyAt('00:00');
+        // $schedule->command('app:calculate-working-hourse')->everyMinute();  // For testing
     }
 
     /**
